f6a536c572be101dcf6a2e1a8daca448f7ce5a0a
mokha <mokha@cisco.com>
2019-04-15 11:28:06 -0600

    initial commit

---
+++ lib/baku.rb
@@ -0,0 +1,6 @@
+require "baku/version"
+
+module Baku
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/baku_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Baku do
+  it "has a version number" do
+    expect(Baku::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
